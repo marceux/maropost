06ed73d6a94b48e3ef4b7a811ae95af3eb4713f1
Marco Salazar de Leon <marcos@markorubel.com>
2015-05-14 12:44:13 -0700

    Simplified tests

--- tests/ContactsTest.php
+++ tests/ContactsTest.php
@@ -1,170 +1,28 @@
 <?php
  
-use Marceux\Maropost\Maropost;
+use Maropost\Maropost;
 
 // Load .env variables
 Dotenv::load(__DIR__);
 
 class ContactTest extends PHPUnit_Framework_TestCase {
 
-	/**
-	 * @dataProvider credentialsProvider
-	 */
-	public function testConstruct($acct, $auth)
+	protected $acct;
+	protected $auth;
+	protected $maropost;
+	protected $contacts;
+
+	protected function setUp()
 	{
-		$m = new Maropost($acct, $auth);
-		$c = $m->contacts();
-		$this->assertEquals(get_class($c), 'Marceux\Maropost\Api\Contacts');
+		$this->acct = $_ENV['MAROPOST_ACCT'];
+		$this->auth = $_ENV['MAROPOST_AUTH'];
+		$this->maropost = new Maropost($this->acct, $this->auth);
+		$this->contacts = $this->maropost->contacts();
 	}
 
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testSearch($acct, $auth)
+	public function testSearch()
 	{
-		// Method Args
-		$email = 'test@example.com';
-
-		$m = new Maropost($acct, $auth);
-		$output = "Get: " . $m->baseUrl . "contacts/email.json?auth_token=$auth&contact[email]=" . urlencode($email);
-		$c = $m->contacts();
-		$result = $c->search($email);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testIndex($acct, $auth)
-	{
-		// Method Args
-		$listId = 1;
-
-		$m = new Maropost($acct, $auth);
-		$output = "Get: " . $m->baseUrl . "lists/$listId/contacts.json?auth_token=$auth";
-		$c = $m->contacts();
-		$result = $c->index($listId);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testCreate($acct, $auth)
-	{
-		// Method Args
-		$listId = 1;
-		$contactData = array('test', 'value');
-
-		$m = new Maropost($acct, $auth);
-		$output = "Post: " . $m->baseUrl . "lists/$listId/contacts.json?auth_token=$auth&test=value";
-		$c = $m->contacts();
-		$result = $c->create($listId, $contactData);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testCreateWithoutList($acct, $auth)
-	{
-		// Method Args
-		$contactData = array('test', 'value');
-
-		$m = new Maropost($acct, $auth);
-		$output = "Post: " . $m->baseUrl . "contacts.json?auth_token=$auth&test=value";
-		$c = $m->contacts();
-		$result = $c->createWithoutList($contactData);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testBlank($acct, $auth)
-	{
-		// Method Args
-		$listId = 1;
-
-		$m = new Maropost($acct, $auth);
-		$output = "Get: " . $m->baseUrl . "lists/$listId/contacts/new.json?auth_token=$auth";
-		$c = $m->contacts();
-		$result = $c->blank($listId);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testShow($acct, $auth)
-	{
-		// Method Args
-		$listId = 1;
-		$contactId = 1;
-
-		$m = new Maropost($acct, $auth);
-		$output = "Get: " . $m->baseUrl . "lists/$listId/contacts/$contactId.json?auth_token=$auth";
-		$c = $m->contacts();
-		$result = $c->show($listId, $contactId);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testUpdate($acct, $auth)
-	{
-		// Method Args
-		$listId = 1;
-		$contactId = 1;
-		$contactData = array('test', 'value');		
-
-		$m = new Maropost($acct, $auth);
-		$output = "Put: " . $m->baseUrl . "lists/$listId/contacts/$contactId.json?auth_token=$auth&test=value";
-		$c = $m->contacts();
-		$result = $c->update($listId, $contactId, $contactData);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testUpdateAllLists($acct, $auth)
-	{
-		// Method Args
-		$email = 'test@example.com';
-
-		$m = new Maropost($acct, $auth);
-		$output = "Put: " . $m->baseUrl . "contacts/unsubscribe_all.json?auth_token=$auth&contact[email]=" . urlencode($email);
-		$c = $m->contacts();
-		$result = $c->updateAllLists($email);
-		$this->assertEquals($result, $output);
-	}
-
-	/**
-	 * @depends testConstruct
-	 * @dataProvider credentialsProvider
-	 */
-	public function testDestroy($acct, $auth)
-	{
-		// Method Args
-		$listId = 1;
-		$contactId = 1;
-		$email = 'test@example.com';
-
-		$m = new Maropost($acct, $auth);
-		$output = "Delete: " . $m->baseUrl . "lists/$listId/contacts/$contactId.json?auth_token=$auth&contact[email]=" . urlencode($email);
-		$c = $m->contacts();
-		$result = $c->destroy($listId, $contactId, $email);
-		$this->assertEquals($result, $output);
+		$result = $this->contacts->search($_ENV['CONTACT_SEARCH_EMAIL']);
 	}
 
 	//--- Providers ---///
